a0a734a4f797d0ac80e24aa8ea19b97a2717e9d1
App Academy Student <email@email.com>
2022-03-25 14:37:38 -0400

    finish arguments exercise

--- args.js
+++ args.js
@@ -42,30 +42,78 @@ class Dog {
   }
 }
 
-const markov = new Cat("Markov");
-const pavlov = new Dog("Pavlov");
+// const markov = new Cat("Markov");
+// const pavlov = new Dog("Pavlov");
 
-markov.says("meow", "Ned");
-// Markov says meow to Ned!
-// true
+// markov.says("meow", "Ned");
+// // Markov says meow to Ned!
+// // true
 
-// bind time args are "meow" and "Kush", no call time args
-markov.says.myBind(pavlov, "meow", "Kush")();
-// Pavlov says meow to Kush!
-// true
+// // bind time args are "meow" and "Kush", no call time args
+// markov.says.myBind(pavlov, "meow", "Kush")();
+// // Pavlov says meow to Kush!
+// // true
 
-// no bind time args (other than context), call time args are "meow" and "a tree"
-markov.says.myBind(pavlov)("meow", "a tree");
-// Pavlov says meow to a tree!
-// true
+// // no bind time args (other than context), call time args are "meow" and "a tree"
+// markov.says.myBind(pavlov)("meow", "a tree");
+// // Pavlov says meow to a tree!
+// // true
 
-// bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(pavlov, "meow")("Markov");
-// Pavlov says meow to Markov!
-// true
+// // bind time arg is "meow", call time arg is "Markov"
+// markov.says.myBind(pavlov, "meow")("Markov");
+// // Pavlov says meow to Markov!
+// // true
 
-// no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(pavlov);
-notMarkovSays("meow", "me");
-// Pavlov says meow to me!
-// true
+// // no bind time args (other than context), call time args are "meow" and "me"
+// const notMarkovSays = markov.says.myBind(pavlov);
+// notMarkovSays("meow", "me");
+// // Pavlov says meow to me!
+// // true
+
+function curriedSum(counter) {
+  let sum = 0;
+  return function summation(num) {
+    if(counter > 1){
+      sum += num;
+      counter --;
+      return summation;
+    }
+    else{
+      sum += num;
+      return sum;
+    }
+  };
+}
+
+// const sums = curriedSum(4);
+// console.log(sums(5)(30)(20)(1)); // => 56
+
+Function.prototype.curry = function(numArgs){
+  let that = this;
+  args = [];
+  return function currying(num){
+    if (numArgs > 1) {
+      args.push(num);
+      numArgs--;
+      return currying;
+    } else {
+      args.push(num);
+      return that.apply(null,args);
+    }
+  };
+};
+
+Function.prototype.curry2 = function (numArgs) {
+  let that = this;
+  args = [];
+  return function currying(num) {
+    if (numArgs > 1) {
+      args.push(num);
+      numArgs--;
+      return currying;
+    } else {
+      args.push(num);
+      return that(...args);
+    }
+  };
+};
